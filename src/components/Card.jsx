18ee7ad198abc34e36845c51ebab9da555c8c706
sasta-kro <sai.aike.shwe.tun.aung@gmail.com>
2026-08-17 23:24:32 +0700

    implemented css

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+function GameCard() {
+
+    return (
+        <button
+            className="game-card"
+            type={"button"}
+            onClick={() => {}}
+        >
+            temp
+        </button>
+
+    )
+}
+
+export default GameCard
